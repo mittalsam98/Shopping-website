85884dcb254e6117edc4c2bd8216d1f18139a85e
Sachin <mittalsam98@gmail.com>
2020-03-30 17:16:04 +0530

    added category route

--- controllers/category.js
+++ controllers/category.js
@@ -1,5 +1,5 @@
 const Category=require('../models/category');
-const User=require('../models/user');
+// const User=require('../models/user');
 
 exports.getCategoryById=(req,res,next,id)=>{
     Category.findById(id).exec((err, cate) => {
@@ -10,5 +10,32 @@ exports.getCategoryById=(req,res,next,id)=>{
         }
         req.category = cate;
         next();
+      });
+};
+
+exports.createCategory=(req,res)=>{
+    const category=new Category(req.body);
+    category.save((err,category)=>{
+        if(err){
+            return res.status(400).json({
+                error: "NOT able to save category in DB"
+              });
+        }
+        res.json({category})
+    })
+}
+
+exports.getCategory=(req,res)=>{
+    return res.json(req.category)
+}
+
+exports.getAllCategory=(req,res)=>{
+    Category.find().exec((err, categories) => {
+        if (err) {
+          return res.status(400).json({
+            error: "NO categories found"
+          });
+        }
+        res.json(categories);
       });
 }
--- routes/category.js
+++ routes/category.js
@@ -2,14 +2,31 @@ const express = require("express");
 const router = express.Router();
 
 const {
-    getCategoryById
+    getCategoryById,
+    createCategory,
+    getCategory,
+    getAllCategory
 } = require("../controllers/category");
 const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
-const { getUserById } = require("../controllers/category");
+const { getUserById } = require("../controllers/user");
 
 
 //params
 router.param("userId", getUserById);
 router.param("categoryId", getCategoryById);
 
-module.exports = router;
+
+//create
+router.post(
+    "/category/create/:userId",
+    isSignedIn,
+    isAuthenticated,
+    isAdmin,
+    createCategory
+  );
+  
+  //read
+  router.get("/category/:categoryId", getCategory);
+  router.get("/categories", getAllCategory);
+
+  module.exports = router;
