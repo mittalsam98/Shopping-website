5edc27c58f3a1d410ca0e40f7f8b2d2e7e5bc060
Sachin <mittalsam98@gmail.com>
2020-03-31 23:24:50 +0530

    added all products routes

--- routes/product.js
+++ routes/product.js
@@ -4,6 +4,12 @@ const router=express.Router();
 const{
     getProductByID,
     createProduct,
+    getProduct,
+    photo,
+    updateProduct,
+    deleteProduct,
+    getAllProducts,
+    getAllUniqueCategories
 }=require('../controllers/product')
 
 const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
@@ -16,4 +22,31 @@ router.param('productId',getProductByID);
 router.post('/product/create/:userId',isSignedIn,isAuthenticated,isAdmin,createProduct)
 
 
+// read routes
+router.get("/product/:productId", getProduct);
+router.get("/product/photo/:productId", photo);
+
+//delete route
+router.delete(
+  "/product/:productId/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  deleteProduct
+);
+
+
+//update route
+router.put(
+  "/product/:productId/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  updateProduct
+);
+
+//listing route
+router.get("/products", getAllProducts);
+
+router.get("/products/categories", getAllUniqueCategories);
 module.exports=router
