4db51b7acd2b73b047d87597bb6f92e76f5b923f
Sachin <mittalsam98@gmail.com>
2020-04-01 21:33:48 +0530

    added createOrder and getAllOrders Route

--- routes/order.js
+++ routes/order.js
@@ -7,10 +7,27 @@ const { updateStock } = require("../controllers/product");
 
 const {
     getOrderById,
+    createOrder,
+    getAllOrders
 }=require('../controllers/order')
 
 router.param("userId", getUserById);
 router.param('orderId',getOrderById)
 
+router.post("/order/create/:userId",
+isSignedIn,
+isAuthenticated,
+pushOrderInPurchaseList,
+updateStock,
+createOrder
+);
+
+router.get(
+    "/order/all/:userId",
+    isSignedIn,
+    isAuthenticated,
+    isAdmin,
+    getAllOrders
+);
 
 module.exports=router;
