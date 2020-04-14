d25c941b9a53bf8f0b23fa59be56eb231e268c55
Sachin <mittalsam98@gmail.com>
2020-04-14 18:28:53 +0530

    added addProduct functionality

--- projectfrontend/src/Routes.js
+++ projectfrontend/src/Routes.js
@@ -8,6 +8,7 @@ import PrivateRoute from "./auth/helper/PrivateRoutes";
 import UserDashBoard from "./user/UserDashBoard";
 import AdminDashBoard from "./user/AdminDashBoard";
 import AddCategory from './admin/AddCategory';
+import AddProduct from './admin/AddProduct';
 
 
 const Routes = () => {
@@ -20,6 +21,19 @@ const Routes = () => {
         <PrivateRoute path="/user/dashboard" exact component={UserDashBoard} />
         <AdminRoute path="/admin/dashboard" exact component={AdminDashBoard} />
         <AdminRoute path="/admin/create/category" exact component={AddCategory} />
+        <AdminRoute path="/admin/create/product" exact component={AddProduct} />
+        {/* <AdminRoute
+          path="/admin/categories"
+          exact
+          component={ManageCategories}
+        />
+
+        <AdminRoute path="/admin/products" exact component={ManageProducts} />
+        <AdminRoute
+          path="/admin/product/update/:productId"
+          exact
+          component={UpdateProduct}
+        /> */}
       </Switch>
     </BrowserRouter>
   );
