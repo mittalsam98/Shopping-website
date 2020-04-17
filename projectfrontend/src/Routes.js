eb131f012f46f23aec64f1cf9d7c12e77d121364
Sachin <mittalsam98@gmail.com>
2020-04-17 19:18:09 +0530

    added cart and card file

--- projectfrontend/src/Routes.js
+++ projectfrontend/src/Routes.js
@@ -12,6 +12,7 @@ import AddProduct from './admin/AddProduct';
 import ManageCategories from './admin/ManageCategories';
 import ManageProducts from './admin/ManageProducts';
 import UpdateProduct from './admin/UpdateProduct';
+import Cart from "./core/Cart";
 
 
 const Routes = () => {
@@ -20,17 +21,22 @@ const Routes = () => {
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/signup" exact component={Signup} />
+        <Route path="/cart" exact component={Cart} />
         <Route path="/signin" exact component={Signin} />
         <PrivateRoute path="/user/dashboard" exact component={UserDashBoard} />
         <AdminRoute path="/admin/dashboard" exact component={AdminDashBoard} />
-        <AdminRoute path="/admin/create/category" exact component={AddCategory} />
-        <AdminRoute path="/admin/create/product" exact component={AddProduct} />
-        <AdminRoute path="/admin/products" exact component={ManageProducts} />
+        <AdminRoute 
+        path="/admin/create/category" 
+        exact 
+        component={AddCategory} />
         <AdminRoute
           path="/admin/categories"
           exact
           component={ManageCategories}
         />
+        <AdminRoute path="/admin/create/product" exact component={AddProduct} />
+        
+        <AdminRoute path="/admin/products" exact component={ManageProducts} />
 
         <AdminRoute
           path="/admin/product/update/:productId"
